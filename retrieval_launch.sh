3a5e748f55cfc602b0a1edf3a18015f933cd6b67
rchennnn <ryanchenitis@gmail.com>
2025-03-21 09:45:10 +0000

    hk

--- retrieval_launch.sh
+++ retrieval_launch.sh
@@ -3,7 +3,7 @@ file_path=/home/ubuntu/cold-blossom-29/~/log
 index_file=$file_path/e5_Flat.index
 corpus_file=$file_path/wiki-18.jsonl
 retriever=intfloat/e5-base-v2
-pplx_key=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
+pplx_key=keyhere
 
 # python search_r1/search/retrieval_server.py --index_path $index_file \
 #                                             --corpus_path $corpus_file \
